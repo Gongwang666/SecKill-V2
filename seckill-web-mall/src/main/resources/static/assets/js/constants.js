216ac3815e6462aa48016e56c50266a6a8067313
Gongwang666 <253285277@qq.com>
2018-04-28 16:51:45 +0800

    日志配置

--- seckill-web-mall/src/main/resources/static/assets/js/constants.js
+++ seckill-web-mall/src/main/resources/static/assets/js/constants.js
@@ -1,6 +1,6 @@
 var CONSTANTS = {};
 
-var baseURL = "http://127.0.0.1:6689";
+var baseURL = "http://10.6.196.17/:6689";
 var URLS = {
     HOME:baseURL+"/home",
     INTRODUCTION:baseURL+'/introduction',//商品详情页
